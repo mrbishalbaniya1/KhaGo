2e02731934d6ec2311565c41afc5d1cfab6ed822
apeuni nepal <apeuninepal.com@gmail.com>
2025-07-29 05:12:30 +0000

    dont use like this // Import the functions you need from the SDKs you n

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApmLgIL3KtyN80IBE2Q05iQHLGvVxJKUs",
-  authDomain: "culinaryflow-pr4wn.firebaseapp.com",
-  projectId: "culinaryflow-pr4wn",
-  storageBucket: "culinaryflow-pr4wn.appspot.com",
-  messagingSenderId: "1053523385881",
-  appId: "1:1053523385881:web:2ca345abf0055cf1b5d488"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
